e304795ab05eeda2dd524fa56cea9a732037932f
Elliot Schoenfeld <jeofner@gmail.com>
2018-03-11 19:15:53 -0400

    Game move tests

--- server/src/classes/game/Game.js
+++ server/src/classes/game/Game.js
@@ -1,5 +1,8 @@
 export default class Game {
     _isFinished = false;
+    _columnCount = 7;
+    _rowCount = 6;
+    _maxMoves = this._rowCount * this._columnCount;
     _moveList = [];
 
     numberOfMoves() {
@@ -15,7 +18,21 @@ export default class Game {
     }
 
     recordMove(column){
+        this._validateMove(column);
         this._moveList.push(column);
+    }
+
+    _validateMove(column) {
+        if (column < 1 || column > this._columnCount)
+            throw new Error(`Column ${column} is not a valid move. Please select a column 1 - 7`)
+
+        if (this._moveList.length === this._maxMoves)
+            throw new Error('Game is finished, no more moves possible!')
+            
+        const numPiecesInColumn = this._moveList.filter( x => x === column).length;
+        if (numPiecesInColumn === this._rowCount)
+            throw new Error(`Column ${column} is filled! Please select a different column`)    
+        
     }
 
     isFinished(){
--- server/src/classes/game/Game.test.js
+++ server/src/classes/game/Game.test.js
@@ -8,5 +8,44 @@ describe('Game', function() {
       const newGame = new Game();
       expect(newGame.numberOfMoves()).to.equal(0);
     })
+
+    it('should create a new game even if a game exists', function() {
+      const gameA = new Game();
+      gameA.recordMove(3);
+      const gameB = new Game();
+      expect(gameB.numberOfMoves()).to.equal(0);
+    })
+  })
+
+  describe('Game Moves', function() {
+    it('should have 1 move after recording 1 move', function() {
+      const game = new Game();
+      game.recordMove(2);
+      expect(game.numberOfMoves()).to.equal(1);
+    })
+
+    it('should not let you record a move more than the board width', function() {
+      const game = new Game();
+      expect( () => game.recordMove(8)).to.throw('Column 8 is not a valid move. Please select a column 1 - 7');
+    })
+
+    it('should not allow more moves than vertically than rows', function() {
+      const game = new Game();
+      for (let i = 0; i < 6; i++) {
+          game.recordMove(1);
+      }
+      expect( () => game.recordMove(1)).to.throw('Column 1 is filled! Please select a different column');
+    })
+
+    it('should not allow more moves than possible', function() {
+      const game = new Game();
+      const maxMoves = 7 * 6;
+      for (let i = 0; i < 7; i++) {
+        for ( let j = 0; j < 6; j++){
+          game.recordMove(i+1);
+        }
+      }
+      expect( () => game.recordMove(1)).to.throw('Game is finished, no more moves possible!');
+    })
   })
 })
